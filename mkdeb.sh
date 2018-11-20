94dc658429365a7e5f60f6ad65ad4e048bdd42bf
root <root@nemo.fritz.box>
2018-11-20 20:02:13 +0100

    0.0.1-2

---
+++ mkdeb.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export DEBEMAIL="lan4lano@gmail.com"
+export DEBFULLNAME="Sven Moennich"
+export LOGNAME="Sven Moennich" 
+
+PROGRAMM=mkversion
+
+command -v dh_make >/dev/null 2>&1 || { echo >&2 "I require dh_make but it's not installed.  Aborting."; exit 1; }
+command -v debuild >/dev/null 2>&1 || { echo >&2 "I require debuild but devscripts it's not installed.  Aborting."; exit 1; }
+
+# Version updaten
+CUSTOM_VERSION=$(cat .last_version.number)
+echo "Neue Version: ${CUSTOM_VERSION}"
+
+
+TMPDIR=$(mktemp -d)
+SRCDIR=$(pwd)
+DSTDIR=$TMPDIR/$PROGRAMM-${CUSTOM_VERSION}
+mkdir -p $DSTDIR
+
+cp -r $SRCDIR/* $DSTDIR/
+rm -rf $DSTDIR/.git
+
+cd $DSTDIR
+
+dh_make --indep --createorig --copyright gpl --yes
+
+rm debian/*.ex debian/*.EX debian/README.Debian debian/README.source 
+
+cat <<EOF >>debian/rules
+override_dh_auto_configure:
+    dh_auto_configure -- --prefix=/usr/local --target=arm-linux-gnueabihf  --host=arm-linux-gnueabi CC=/usr/local/bin/arm-linux-gnueabihf-gcc
+EOF
+
+
+cat <<EOF >debian/install
+$PROGRAMM usr/sbin
+EOF
+
+debuild -us -uc
+
+cd ..
+cp *.deb $SRCDIR
+
+rm -rf $TMPDIR
